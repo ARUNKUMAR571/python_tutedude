e26ef90ca5c61d7b23d274c5a1005181d22ca2ef
ARUNKUMAR571 <arunkumar.sodad571@gmail.com>
2025-05-01 12:26:39 +0530

    initial commit

---
+++ math_module.py
@@ -0,0 +1,16 @@
+
+# import math
+# print(math.pi)
+#
+# from math import pi
+# print(pi)
+#
+# from math import *
+# print(pi)
+
+
+import math
+n = int(input("Enter the number : "))
+print("Square root: ",math.sqrt(n))
+print("Logarithm: ",math.log(n))
+print("Sine: ",math.sin(n))
